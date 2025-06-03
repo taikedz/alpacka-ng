a079e46bca555dec8b42e6cbb66b1a24bc750064
Tai Kedzierski <dch.tai@gmail.com>
2025-06-03 16:51:05 +0100

    Add zypper support

---
+++ pakang/pkg-zypper.go
@@ -0,0 +1,65 @@
+package pakang
+
+type ZypperPM struct {
+    extraflags []string
+}
+
+func NewZypperPM(flags []string) ZypperPM {
+    return ZypperPM{flags}
+}
+
+func (self ZypperPM) Help() []string {
+    return []string{
+        "clean : Clean the cache",
+        "fix : fix broken dependencies",
+    }
+}
+
+func (self ZypperPM) Search(terms []string) {
+    cmd := []string{"zypper", "search"}
+    cmd = append(cmd, terms...)
+    RunCmd(0, cmd...)
+}
+
+func (self ZypperPM) Extra(terms []string) {
+    if ArrayHas("clean", self.extraflags) {
+        self.clean()
+    } else {
+        self.Search(terms)
+    }
+}
+
+func (self ZypperPM) clean() {
+    RunCmd(NEED_ROOT, "zypper", "clean")
+}
+
+func (self ZypperPM) Show(pkg string) {
+    RunCmd(0, "zypper", "info", pkg)
+}
+
+func (self ZypperPM) Update() {
+    RunCmd(NEED_ROOT, "zypper", "refresh")
+}
+
+func (self ZypperPM) Install(yes bool, packages []string) {
+    cmd := []string{"zypper", "install"}
+    if yes {
+        cmd = append(cmd, "-y")
+    }
+    cmd = append(cmd, packages...)
+    RunCmd(NEED_ROOT, cmd...)
+}
+
+func (self ZypperPM) Remove(packages []string) {
+    cmd := []string{"zypper", "remove"}
+    cmd = append(cmd, packages...)
+    RunCmd(NEED_ROOT, cmd...)
+}
+
+func (self ZypperPM) Upgrade(yes bool) {
+    cmd := []string{"zypper", "update"}
+    if yes {
+        cmd = append(cmd, "-y")
+    }
+    RunCmd(NEED_ROOT, cmd...)
+}
