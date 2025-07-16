85529ffd0028a987a8214496d799700366038162
Tai Kedzierski <dch.tai@gmail.com>
2025-07-16 16:55:54 +0100

    Bump version

--- pakang/version.go
+++ pakang/version.go
@@ -5,7 +5,7 @@ import "fmt"
 // update readme too ;-)
 const VER_MAJ int = 1
 const VER_MIN int = 0
-const VER_PATCH int = 0
+const VER_PATCH int = 1
 
 func GetVersionString() string {
 	return fmt.Sprintf("v%d.%d.%d", VER_MAJ, VER_MIN, VER_PATCH)
