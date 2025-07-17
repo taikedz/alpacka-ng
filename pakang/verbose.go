3fb0ac9f72e3b6b82ad8635e364788824206c1fb
Tai Kedzierski <dch.tai@gmail.com>
2025-07-17 19:21:48 +0100

    Add correction to NO_COLOR detection

--- pakang/verbose.go
+++ pakang/verbose.go
@@ -17,7 +17,13 @@ func printVerbose(template string, items ...any) {
 }
 
 func setColor(val string) {
-	if val, isset := os.LookupEnv("NOCOLOR"); isset && val != "false" {
+	// https://no-color.org/
+	/*
+		Command-line software which adds ANSI color to its output by default should check
+		for a NO_COLOR environment variable that, when present and not an empty string
+		(regardless of its value), prevents the addition of ANSI color.
+	*/
+	if val, isset := os.LookupEnv("NO_COLOR"); isset && len(val) > 0 {
 		return
 	}
 	fmt.Printf("\033[%sm", val)
