3e8a8561c314212b315fc890aa0c813d38c8f4f3
Tai Kedzierski <dch.tai@gmail.com>
2025-07-30 15:42:03 +0100

    Update build, do not depend on containerisation

--- build.sh
+++ build.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
-BUILD=(go build -o bin/paf paf.go)
-OCI=(run -i --rm -v "$PWD:/paf-build" -v "$PWD/gocache:/go" golang:1.24 sh -c "cd /paf-build; ${BUILD[*]}")
-
-
 HERE="$(dirname "$0")"
 cd "$HERE"
 
-mkdir -p gocache
+go build -trimpath -o bin/paf paf.go
 
-has() { which "$1" 2>&1 >/dev/null ; }
-doit() { (set -x ; "$@") ; }
-
-if has docker; then
-    doit docker "${OCI[@]}"
-elif has podman; then
-    doit podman "${OCI[@]}"
-fi
